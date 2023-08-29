db6ba59c8448ccb698c17b6de3be1471e29da347
SouaOui <soumiaouzat@gmail.com>
2023-08-29 22:13:52 +0100

    insert to left of parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,22 +3,23 @@
  * binary_tree_insert_left - function that insert left to a node
  * @parent: parent of the inserted node
  * @value: value of the inserted node
- * Return: 
+ * Return: the pointer to the node added
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node, *save;
 
-    if (parent == NULL)
-        return (NULL);
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
-    if (parent->left  != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
-    parent->left = new_node;
-    return (new_node);
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->left  != NULL)
+	{
+		save = parent->left;
+		new_node->left = save;
+		save->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
 }
