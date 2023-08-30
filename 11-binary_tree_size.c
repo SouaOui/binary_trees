6b8380c84e8039b5e0da7928e3ff72bd4b44398b
zak <zhaci001@gmail.com>
2023-08-30 17:44:45 +0100

    printing by us

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,14 +1,13 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t count = 1;
+    
     if (tree == NULL)
             return 0;
+    size_t count = 1;
     if(!tree->left || !tree->right)
     {
         count += binary_tree_size(tree->left);
         count += binary_tree_size(tree->right);
-
     }
     return count;
-
 }
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,6 @@
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+ if (tree == NULL)
+    return 0;
+
+}
