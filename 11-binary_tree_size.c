864fb06cb971c242083c7c7d008a93c1c355ad2e
SouaOui <soumiaouzat@gmail.com>
2023-08-31 02:59:11 +0100

    Betty Fixed

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,15 +6,15 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t count;
+	size_t count;
 
-    if (tree == NULL)
-        return (0);
-    count = 1;
-    if (tree->left || tree->right)
-    {
-        count += binary_tree_size(tree->left);
-        count += binary_tree_size(tree->right);
-    }
-    return (count);
+	if (tree == NULL)
+		return (0);
+	count = 1;
+	if (tree->left || tree->right)
+	{
+		count += binary_tree_size(tree->left);
+		count += binary_tree_size(tree->right);
+	}
+	return (count);
 }
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,14 +6,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t count;
+	size_t count;
 
-    if (tree == NULL)
-        return (0);
-    if (!tree->left && !tree->right)
-        return (1);
-    count = 0;
-    count += binary_tree_leaves(tree->left);
-    count += binary_tree_leaves(tree->right);
-    return (count);
+	if (tree == NULL)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	count = 0;
+	count += binary_tree_leaves(tree->left);
+	count += binary_tree_leaves(tree->right);
+	return (count);
 }
