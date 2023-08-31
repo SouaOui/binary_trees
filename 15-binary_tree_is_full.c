41869dab7cea34fb217e43af3a9a0911f236bdd0
SouaOui <soumiaouzat@gmail.com>
2023-08-31 11:25:12 -0700

    Is it Full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,17 +6,12 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-size_t count1;
-size_t count2;
-if (tree == NULL)
-return (1);
-if ((tree->left != NULL && tree->right != NULL) ||
-(tree->left == NULL && tree->right == NULL))
-{
-count1 = binary_tree_is_full(tree->left);
-count2 = binary_tree_is_full(tree->right);
-return (count1 && count2);
-}
-return (0);
+	if (tree == NULL)
+		return (0);
+	if ((tree->left == NULL && tree->right == NULL))
+		return (1);
+	if ((tree->left != NULL && tree->right != NULL))
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
 
