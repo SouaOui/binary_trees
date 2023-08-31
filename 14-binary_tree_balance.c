c90dd65f7840ed8453c6c12f5aabd60752f7de60
zak <zhaci001@gmail.com>
2023-08-31 15:22:45 +0100

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,19 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - function that return factor of balance
- * @tree: pointer to the root of the tree
- * Return: integer
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node
+ * Return: the result
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t count1;
-	size_t count2;
+int countLeft = 0, countRight = 0;
 
-	if (tree == NULL)
-		return (0);
+if (tree == NULL)
+return (0);
 
-	count1 = binary_tree_balance(tree->left);
-	count2 = binary_tree_balance(tree->right);
-	return (count1 - count2 + 1);
+if (tree->left != NULL)
+countLeft = binary_tree_height(tree->left) + 1;
+if (tree->right != NULL)
+countRight = binary_tree_height(tree->right) + 1;
+return (countLeft - countRight);
 }
