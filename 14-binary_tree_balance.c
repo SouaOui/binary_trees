8a875e2c29537b182b03222dd0b24e52e96a6186
zak <zhaci001@gmail.com>
2023-08-31 15:01:56 +0100

    blance factore

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,21 +1,19 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - function  that measures the balance factor of a binary tree
+ * binary_tree_balance -that measures the balance factor of a binary tree
  * @tree: pointer to the root node
- * Return: the result 
+ * Return: the result
  */
-int binary_tree_balance(const binary_tree_t *tree){
-    int count1;
-    int count2;
-    if (tree == NULL)
-        return 1;
-    count1 = 0;
-    count2 = 0;
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        count1 += binary_tree_balance(tree->left);
-        count2 += binary_tree_balance(tree->right);
-        return (count1 - count2);
-    }
-    return 0;
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int countLeft = 0, countRight = 0;
+
+if (tree == NULL)
+return (0);
+
+if (tree->left != NULL)
+countLeft = binary_tree_height(tree->left) + 1;
+if (tree->right != NULL)
+countRight = binary_tree_height(tree->right) + 1;
+return (countLeft - countRight);
 }
