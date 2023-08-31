d24a4f5bfc6eb05673ede6dfe47bd40a5d2c90d1
SouaOui <soumiaouzat@gmail.com>
2023-08-31 15:01:20 -0700

    Depth of tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,28 +1,16 @@
 #include "binary_trees.h"
-/**
- * max - returns max of two values
- * @value1: value 1
- * @value2: value 1
- * Return: the maximum
- */
-size_t max(size_t value1, size_t value2)
-{
-	if (value1 < value2)
-		return (value2);
-	return (value1);
-}
-/**
- * binary_tree_height - function that calculate the height of a binary tree
- * @tree: pointer to the root of the tree
- * Return: positive number or 0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL || (tree->left != NULL && tree->right != NULL))
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree.
+ * @tree: A pointer to the node to measure the depth.
+ *
+ * Return: If tree is NULL, your function must return 0, else return the depth.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (max(left_height, right_height) + 1);
+	if (tree != NULL && tree->parent != NULL)
+		return (1 + binary_tree_depth(tree->parent));
+	return (0);
 }
