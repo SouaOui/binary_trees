6ad2f8cab92450a2c51a18378002c058d5dae03b
zak <zhaci001@gmail.com>
2023-08-31 15:32:58 +0100

    blance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -36,11 +36,11 @@ size_t max(size_t value1, size_t value2)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height = 0, right_height = 0;
+size_t left_height = 0, right_height = 0;
 
-        if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-                return (0);
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
-        return (max(left_height, right_height) + 1);
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+return (0);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+return (max(left_height, right_height) + 1);
 }
