ec3b1ed8e95520cbf2461687f29f027f5a3edc4e
zak <zhaci001@gmail.com>
2023-08-31 12:14:59 +0100

    i update the task 15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,14 @@
-int binary_tree_balance(const binary_tree_t *tree)
-{
+
+int binary_tree_is_full(const binary_tree_t *tree){
     size_t count1;
     size_t count2;
     if (tree == NULL)
-        return 0;
-
-    count1 = binary_tree_balance(tree->left);
-    count2 = binary_tree_balance(tree->right);
-    return (count1 - count2) + 1;
+        return 1;
+    if ((tree->lef != NULL && tree->right != NULL) || (tree->lef === NULL && tree->right == NULL))
+    {
+        count1 = binary_tree_is_full(tree->left);
+        count2 = binary_tree_is_full(tree->right);
+        return (count1 && count2) 
+    }
+    return 0;
 }
