ee06fc5c4f3aed9cc820da72b77a6de3b312ae33
SouaOui <soumiaouzat@gmail.com>
2023-08-31 04:07:47 +0100

    Documentation Betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,8 +8,8 @@
 size_t max(size_t value1, size_t value2)
 {
 	if (value1 < value2)
-		return value2;
-	return value1;
+		return (value2);
+	return (value1);
 }
 /**
  * binary_tree_height - function that calculate the height of a binary tree
@@ -20,8 +20,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL || (tree->left != NULL && tree->right != NULL)) 
-		return 0;
+	if (tree == NULL || (tree->left != NULL && tree->right != NULL))
+		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	return (max(left_height, right_height) + 1);
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - function that return factor of balance
+ * @tree: pointer to the root of the tree
+ * Return: integer
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    size_t count1;
-    size_t count2;
-    if (tree == NULL)
-        return 0;
+	size_t count1;
+	size_t count2;
 
-    count1 = binary_tree_balance(tree->left);
-    count2 = binary_tree_balance(tree->right);
-    return (count1 - count2) + 1;
+	if (tree == NULL)
+		return (0);
+
+	count1 = binary_tree_balance(tree->left);
+	count2 = binary_tree_balance(tree->right);
+	return (count1 - count2 + 1);
 }
