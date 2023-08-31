e03a034a7ec507f07a454895d864a7c478e5ec85
zak <zhaci001@gmail.com>
2023-08-31 01:22:24 +0100

    printing by us

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,13 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that calculate the size of the tree
+ * @tree: pointer to root of the tree
+ * Return: size of the tree
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
     size_t count;
+
     if (tree == NULL)
-            return 0;
+        return (0);
     count = 1;
-    if(!tree->left || !tree->right)
+    if (tree->left || tree->right)
     {
         count += binary_tree_size(tree->left);
         count += binary_tree_size(tree->right);
     }
-    return count;
+    return (count);
 }
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,11 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that calculate the size of the tree
+ * @tree: pointer to root of the tree
+ * Return: number of leaves
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
-{ 
+{
     size_t count;
+
     if (tree == NULL)
-        return 0;
-    count =0;
-    if (tree->left == NULL || tree->right == NULL)
-       /**perhaps,, logicaly itsnot correct */ count += binary_tree_leaves(tree->left);
-                                                count += binary_tree_leaves(tree->right);
-    return count;
+        return (0);
+    if (!tree->left && !tree->right)
+        return (1);
+    count = 0;
+    count += binary_tree_leaves(tree->left);
+    count += binary_tree_leaves(tree->right);
+    return (count);
 }
