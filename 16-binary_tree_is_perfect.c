c2148448bf1c2dc217093392be6482419966d798
SouaOui <soumiaouzat@gmail.com>
2023-09-01 09:20:22 -0700

    Update Perfect check with height

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,52 +1,42 @@
 #include "binary_trees.h"
-/**
- * binary_tree_is_perfect -checks if a binary tree is perfect
- * @tree: pointer to the root node
- * Return: the result
- */
 
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node
+ * Return: 1 if perfect, 0 otherwise
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int countleft = 0, countright = 0;
+	size_t left_height, right_height;
 
-if (tree == NULL)
-return (0);
-if (tree->left != NULL && tree->right != NULL)
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (left_height == right_height &&
+	binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_height - returns the height of a binary tree
+ * @tree: pointer to the root node
+ * Return: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-    countleft = binary_tree_depth(tree->left);
-    countright = binary_tree_depth(tree->right);
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
-if (countleft == countright)
-{
-    return (1);
-}
-return (0);
-}
-size_t binary_tree_height(const binary_tree_t *tree){
-size_t count1, count2;
-if (tree ==NULL || (tree->left == NULL && tree->right == NULL))
-{
-    return (0);
-}
-count1 = 0;
-count2 = 0;
-if (tree->left != NULL || tree->right != NULL)
-{
-    count1 = binary_tree_height(tree->left);
-    count2 = binary_tree_height(tree->right);
-}
-if (count1 > count2)
-{
-    return (count1 + 1);
-}
-return (count2 + 1);
-}
-/*dept*/
- size_t binary_tree_depth(const binary_tree_t *tree)
- {
- if (tree == NULL)
-     return (0);
- if (tree->parent !=NULL && tree != NULL)
-     return (1+binary_tree_depth(tree->parent));
- return (0);
- }
+
