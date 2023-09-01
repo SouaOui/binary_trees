7866d9cea35fe84d30d3410a1a562e83648995c4
Zack-hc <zhaci001@gmail.com>
2023-09-01 08:36:28 -0700

    perfect-task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,49 +4,49 @@
  * @tree: pointer to the root node
  * Return: the result
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int countleft, countright;
+int countleft = 0, countright = 0;
 
-	if (tree == NULL)
-		return (0);
-	countleft = 0;
-	countright = 0;
-	countleft += binary_tree_height(tree->left);
-	countright += binary_tree_height(tree->right);
-	if (countleft == countright)
-	{
-		if (tree->left != NULL && tree->right != NULL)
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-/**
- * binary_tree_height -the hight of a binary tree
- * @tree: pointer to the root node
- * Return: the result
- */
-size_t binary_tree_height(const binary_tree_t *tree)
+if (tree == NULL)
+return (0);
+if (tree->left != NULL && tree->right != NULL)
 {
-	size_t count1, count2;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-	{
-		return (0);
-	}
-	count1 = 0;
-	count2 = 0;
-	if (tree->left != NULL || tree->right != NULL)
-	{
-		count1 = binary_tree_height(tree->left);
-		count2 = binary_tree_height(tree->right);
-	}
-	if (count1 > count2)
-	{
-		return (count1 + 1);
-	}
-	return (count2 + 1);
+    countleft = binary_tree_depth(tree->left);
+    countright = binary_tree_depth(tree->right);
 }
-
+if (countleft == countright)
+{
+    return (1);
+}
+return (0);
+}
+size_t binary_tree_height(const binary_tree_t *tree){
+size_t count1, count2;
+if (tree ==NULL || (tree->left == NULL && tree->right == NULL))
+{
+    return (0);
+}
+count1 = 0;
+count2 = 0;
+if (tree->left != NULL || tree->right != NULL)
+{
+    count1 = binary_tree_height(tree->left);
+    count2 = binary_tree_height(tree->right);
+}
+if (count1 > count2)
+{
+    return (count1 + 1);
+}
+return (count2 + 1);
+}
+/*dept*/
+ size_t binary_tree_depth(const binary_tree_t *tree)
+ {
+ if (tree == NULL)
+     return (0);
+ if (tree->parent !=NULL && tree != NULL)
+     return (1+binary_tree_depth(tree->parent));
+ return (0);
+ }
