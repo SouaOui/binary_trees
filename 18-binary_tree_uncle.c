b00a1829056a2be2a954a622772a1d9f44e43bb5
Zack-hc <zhaci001@gmail.com>
2023-08-31 18:27:40 -0700

    deleting the 18-main and 17-main and pushing find my uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/**
+ * binary_tree_uncle -  where is my uncle if im a node
+ * @node: a pointer to me to find my uncle
+ * Return: a pointer to my uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ * Return: a pointer to the sibling node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->right == node)
+		return (node->parent->left);
+
+	return (node->parent->right);
+}
